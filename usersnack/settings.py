324392733308149fa00681de8fe6b7843ae6d073
Aref Yazdi <arefyazdi4@gmail.com>
2022-11-29 11:23:30 +0330

    Config CORS

--- usersnack/settings.py
+++ usersnack/settings.py
@@ -50,12 +50,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS config
+CORS_ALLOW_ALL_ORIGINS = True
+ALLOWED_HOSTS += ['*']
 
 ROOT_URLCONF = 'usersnack.urls'
 
